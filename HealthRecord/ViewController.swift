66113b4334c4e1f48bf3b71f410d5daeb8b4cb63
Elbat.Mesfin <elbat.mesfin@ElbatMesfins-MacBook-Pro.local>
2019-08-20 11:20:57 -0400

    first commit

---
+++ HealthRecord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthRecord
+//
+//  Created by Elbat.Mesfin on 2019-08-20.
+//  Copyright © 2019 Elbat.Mesfin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
